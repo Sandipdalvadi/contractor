7b51bc5baa5654ba8fe3391e56ad51b8e3309247
sandip_dalvadi <dalvadisandip123@gmail.com>
2020-08-20 16:16:07 +0530

    create new txxt file

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://globearchitectures.com'),
+    'url' => env('APP_URL', 'http://globearchitectures.com'),
 
     'asset_url' => env('ASSET_URL', null),
 
